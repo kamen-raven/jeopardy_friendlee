91f3b7d6f423a6a9790bd8aee3078a6823e370cc
Slava Kamenev <slava-stavropol@yandex.ru>
2025-12-29 11:40:46 +0300

    feat: start screen

--- src/components/StartScreen/StartScreen.tsx
+++ src/components/StartScreen/StartScreen.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import styles from './StartScreen.module.scss';
 import FriendLeeLogo from '../../assets/logo.svg';
 import ArrowIcon from '@assets/arrow.svg';
+import FroggyImage from '@assets/froggy.png';
+import Image from 'next/image';
 
 export interface StartGameInterface {
   onStartGame: () => void;
@@ -14,23 +16,30 @@ export interface StartGameInterface {
 const StartScreen: React.FC<StartGameInterface> = ({ onStartGame, isAnimating = false }) => {
   return (
     <div className={`${styles.startGame} ${isAnimating ? styles.animating : ''}`}>
-      <div className={styles.logo}>
-        <FriendLeeLogo />
+      <div className={styles.wrapper}>
+
+        <div className={styles.logo}>
+          <FriendLeeLogo />
+        </div>
+
+
+        <h1 className={`${styles.title} ${isAnimating ? styles.animating : ''}`}>
+          С <span className={styles.title__highlight}>Новым</span> Годом!
+        </h1>
+
+        <button
+          className={`${styles.startButton} ${isAnimating ? styles.animating : ''}`}
+          onClick={onStartGame}
+          disabled={isAnimating}
+        >
+          Начать игру
+          <ArrowIcon />
+        </button>
       </div>
 
-
-      <h1 className={`${styles.title} ${isAnimating ? styles.animating : ''}`}>
-        С <span className={styles.title__highlight}>Новым</span> Годом!
-      </h1>
-
-      <button
-        className={`${styles.startButton} ${isAnimating ? styles.animating : ''}`}
-        onClick={onStartGame}
-        disabled={isAnimating}
-      >
-        Начать игру
-        <ArrowIcon />
-      </button>
+      <div className={styles.froggy}>
+        <Image src={FroggyImage} alt="Froggy" />
+      </div>
 
     </div>
   );
